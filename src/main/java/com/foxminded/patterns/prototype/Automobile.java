03e79cad24b4e47233f33d0cbe5e3d2e09528422
Denys Lenskyi <denislensky94@gmail.com>
2024-02-16 14:39:31 -0500

    Refactoring. Singleton vanished

--- src/main/java/com/foxminded/patterns/prototype/Automobile.java
+++ src/main/java/com/foxminded/patterns/prototype/Automobile.java
@@ -2,47 +2,58 @@ package com.foxminded.patterns.prototype;
 
 public class Automobile implements Cloneable {
 
+  private static final String BRAND = "Denys Lenskyi Custom Motors";
+  private static final String SHOP_NAME = "Lenskyi Corner Store";
+  private static final int YEAR = 2024;
+  private static final Automobile PASSENGER =
+      new Automobile(Type.PASSENGER, BRAND, SHOP_NAME, YEAR);
+  private static final Automobile TRUCK = new Automobile(Type.TRUCK, BRAND, SHOP_NAME, YEAR);
+  private static final Automobile MINIVAN = new Automobile(Type.MINIVAN, BRAND, SHOP_NAME, YEAR);
+
   private int number;
   private String colour;
   private Type type;
   private String brand;
   private int year;
   private String shopName;
+
   // etc
 
-  private static Automobile instance;
-
-  static {
-    instance = new Automobile();
-    instance.setBrand("Denys Lenskyi Custom Motors");
-    instance.setYear(2024);
-    instance.setShopName("Lenskyi Corner Store");
+  private Automobile(Type type, String brand, String shopName, int year) {
+    this.type = type;
+    this.brand = brand;
+    this.shopName = shopName;
+    this.year = year;
   }
 
-  private Automobile() {}
-
-  public static Automobile getPassenger(int number, String colour) throws CloneNotSupportedException {
-    Automobile automobile = (Automobile) instance.clone();
-    automobile.setType(Type.PASSENGER);
+  public static Automobile getPassenger(int number, String colour) {
+    Automobile automobile = PASSENGER.clone();
     automobile.setNumber(number);
     automobile.setColour(colour);
     return automobile;
   }
 
-  public static Automobile getTruck(int number, String colour) throws CloneNotSupportedException {
-    Automobile automobile = (Automobile) instance.clone();
-    automobile.setType(Type.TRUCK);
+  public static Automobile getTruck(int number, String colour) {
+    Automobile automobile = TRUCK.clone();
     automobile.setNumber(number);
     automobile.setColour(colour);
     return automobile;
   }
 
-  public static Automobile getMinivan(int number, String colour) throws CloneNotSupportedException {
-    Automobile automobile = (Automobile) instance.clone();
-    automobile.setType(Type.MINIVAN);
+  public static Automobile getMinivan(int number, String colour) {
+    Automobile automobile = MINIVAN.clone();
     automobile.setNumber(number);
     automobile.setColour(colour);
     return automobile;
+  }
+
+  @Override
+  protected Automobile clone() {
+    try {
+      return (Automobile) super.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public void setNumber(int number) {
@@ -51,26 +62,6 @@ public class Automobile implements Cloneable {
 
   public void setColour(String colour) {
     this.colour = colour;
-  }
-
-  public Type getType() {
-    return type;
-  }
-
-  private void setType(Type type) {
-    this.type = type;
-  }
-
-  private void setBrand(String brand) {
-    this.brand = brand;
-  }
-
-  private void setYear(int year) {
-    this.year = year;
-  }
-
-  private void setShopName(String shopName) {
-    this.shopName = shopName;
   }
 
   @Override
--- src/main/java/com/foxminded/patterns/prototype/Main.java
+++ src/main/java/com/foxminded/patterns/prototype/Main.java
@@ -1,7 +1,7 @@
 package com.foxminded.patterns.prototype;
 
 public class Main {
-  public static void main(String[] args) throws CloneNotSupportedException {
+  public static void main(String[] args) {
     Automobile pass = Automobile.getPassenger(1, "black");
     Automobile truck = Automobile.getTruck(2, "blue");
     Automobile mini = Automobile.getMinivan(3, "red");
