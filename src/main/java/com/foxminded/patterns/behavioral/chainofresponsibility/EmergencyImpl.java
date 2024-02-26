abec06cfdbf59d232118d1675f7141c16f12e672
Denys Lenskyi <denislensky94@gmail.com>
2024-02-26 15:47:46 -0500

    Refactoring

--- src/main/java/com/foxminded/patterns/behavioral/chainofresponsibility/EmergencyImpl.java
+++ src/main/java/com/foxminded/patterns/behavioral/chainofresponsibility/EmergencyImpl.java
@@ -1,15 +1,21 @@
 package com.foxminded.patterns.behavioral.chainofresponsibility;
 
-import com.foxminded.patterns.behavioral.chainofresponsibility.emergency.EmergencyCodeHandler;
+import com.foxminded.patterns.behavioral.chainofresponsibility.emergency.CriminalEmergencyCodeHandler;
+import com.foxminded.patterns.behavioral.chainofresponsibility.emergency.EmergencyCodeChainHandler;
+import com.foxminded.patterns.behavioral.chainofresponsibility.emergency.FireEmergencyCodeHandler;
+import com.foxminded.patterns.behavioral.chainofresponsibility.emergency.HealthEmergencyCodeHandler;
 import com.foxminded.patterns.behavioral.chainofresponsibility.model.EmergencyRequest;
 
 public class EmergencyImpl implements Emergency {
 
-  private EmergencyCodeHandler emergencyCodeHandler;
+  private EmergencyCodeChainHandler emergencyCodeChainHandler;
 
   @Override
   public void getResponse(EmergencyRequest request) {
-    emergencyCodeHandler = new EmergencyCodeHandler();
-    emergencyCodeHandler.processRequest(request).forEach(System.out::println);
+    emergencyCodeChainHandler = new EmergencyCodeChainHandler();
+    emergencyCodeChainHandler.addHandler(new CriminalEmergencyCodeHandler());
+    emergencyCodeChainHandler.addHandler(new FireEmergencyCodeHandler());
+    emergencyCodeChainHandler.addHandler(new HealthEmergencyCodeHandler());
+    emergencyCodeChainHandler.processRequest(request).forEach(System.out::println);
   }
 }
