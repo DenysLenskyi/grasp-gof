0ea462cebf89686262892a3bef34a6bdadcf837e
Denys Lenskyi <denislensky94@gmail.com>
2024-02-27 13:42:48 -0500

    Interpreter initial commit

--- src/main/java/com/foxminded/patterns/behavioral/command/Pizza.java
+++ src/main/java/com/foxminded/patterns/behavioral/command/Pizza.java
@@ -6,11 +6,20 @@ import java.util.List;
 public class Pizza {
   private List<String> ingredients;
 
+  public Pizza() {}
+
   public Pizza(List<String> ingredients) {
     this.ingredients = ingredients;
   }
 
   public void printIngredients() {
     this.ingredients.forEach(System.out::println);
+  }
+
+  public void addIngredient(String ingredient) {
+    if (this.ingredients == null) {
+      ingredients = new ArrayList<>();
+    }
+    this.ingredients.add(ingredient);
   }
 }
---
+++ src/main/java/com/foxminded/patterns/behavioral/interpreter/PizzaInterpreterImpl.java
@@ -0,0 +1,48 @@
+package com.foxminded.patterns.behavioral.interpreter;
+
+import com.foxminded.patterns.behavioral.command.Pizza;
+import com.foxminded.patterns.behavioral.command.Waiter;
+
+import java.util.List;
+
+/**
+ * input pattern: "--pizza_name=<name>
+ * --add_ingredients=<ingredient1>,<ingredient2>,<ingredient3>..."
+ */
+public class PizzaInterpreterImpl implements PizzaInterpreter {
+
+  @Override
+  public Pizza cookCustomPizza(String recipe) {
+    Pizza pizza = null;
+    Waiter waiter = new Waiter();
+    RecipeParser recipeParser = new RecipeParser();
+    CustomPizzaRecipe customPizzaRecipe = recipeParser.parse(recipe);
+
+    switch (customPizzaRecipe.getPizzaName()) {
+      case "Margo" ->
+          pizza =
+              makeCustomPizza(
+                  waiter, customPizzaRecipe.getPizzaName(), customPizzaRecipe.getIngredients());
+
+      case "Fungi" ->
+          pizza =
+              makeCustomPizza(
+                  waiter, customPizzaRecipe.getPizzaName(), customPizzaRecipe.getIngredients());
+      case "Marinara" ->
+          pizza =
+              makeCustomPizza(
+                  waiter, customPizzaRecipe.getPizzaName(), customPizzaRecipe.getIngredients());
+      default -> {
+        pizza = new Pizza();
+        customPizzaRecipe.getIngredients().forEach(pizza::addIngredient);
+      }
+    }
+    return pizza;
+  }
+
+  private Pizza makeCustomPizza(Waiter waiter, String name, List<String> ingredients) {
+    Pizza pizza = waiter.getCommand(name).cookPizza();
+    ingredients.forEach(pizza::addIngredient);
+    return pizza;
+  }
+}
